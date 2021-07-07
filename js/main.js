e49d63891f025fc843b73de7729866191b9423a0
Maria Emilia Oliveira <oliveiramariaemilia2@gmail.com>
2021-07-07 19:49:50 -0300

    array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
-var nome="Maria Emília Oliveira";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo";
+var frutas = [{nome:"maçã" , cor:"vermelha"} , {nome:"kiwi" , cor:"verde"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*var fruta = {nome:"maçã" , cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã" , "pêra" , "laranja"];
+//lista.pop();
+//lista.push("kiwi");
+//console.log(lista.join(" - "));
+//console.log(lista.toString());
+//alert(lista);
+
+
+//var nome="Maria Emília Oliveira";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
 //alert( nome  + " tem " +  idade  + " anos");
 //alert (idade  +  idade2);
-console.log(nome);
-console.log(n1  *  n2);
-console.log(frase.replace("Japão" ,  "Brasil"));
+//console.log(nome);
+//console.log(n1  *  n2);
+//console.log(frase.replace("Japão" ,  "Brasil"));
 
 //alert(frase.replace("Japão" ,  "Brasil"));
