cfb4f0670ec71e656522c38c3ebe6bb5da18dfcb
Maria Emilia Oliveira <oliveiramariaemilia2@gmail.com>
2021-07-07 20:26:01 -0300

    condicionais, laços de repetição e data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
-var frutas = [{nome:"maçã" , cor:"vermelha"} , {nome:"kiwi" , cor:"verde"}]
-console.log(frutas);
-alert(frutas[1].nome);
+var d = new Date();
+alert(d.getMinutes() + 1);
+alert(d.getTime);
+alert(d.getUTCHours);
+
+
+/*var count;
+for(count = 0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*var idade = prompt("Qual a sua idade?");
+if(idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+
+
+
+//var frutas = [{nome:"maçã" , cor:"vermelha"} , {nome:"kiwi" , cor:"verde"}]
+//console.log(frutas);
+//alert(frutas[1].nome);
 
 
 /*var fruta = {nome:"maçã" , cor:"vermelha"}
